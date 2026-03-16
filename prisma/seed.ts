91fcc3b9973b4487f728ec4415c6b55d5cb6f775
irvan <irvanyusufcahyadi4@gmail.com>
2026-03-16 14:29:44 +0700

    update seed for added avatar profiles

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -12,6 +12,18 @@ const adapter = new PrismaMariaDb({
 
 const prisma = new PrismaClient({ adapter });
 
+const AVATAR_PROFILES = [
+  "https://res.cloudinary.com/dfsozazph/image/upload/v1770360204/iibzj8togx6jwprvumoq.png",
+  "https://res.cloudinary.com/dfsozazph/image/upload/v1770360202/ucr7kccqxjeu4ue5fjqw.png",
+  "https://res.cloudinary.com/dfsozazph/image/upload/v1770360202/xcirghesfin7ahv1zhj7.png",
+  "https://res.cloudinary.com/dfsozazph/image/upload/v1770360201/v5wmqqgz0pwdbr8iljzf.png",
+  "https://res.cloudinary.com/dfsozazph/image/upload/v1770360200/s3sooyorbbljiq5m9trw.png",
+  "https://res.cloudinary.com/dfsozazph/image/upload/v1770360199/bbrbzaau7yw7oonujehq.png",
+  "https://res.cloudinary.com/dfsozazph/image/upload/v1770360196/zasbhgx1vfhmv3jvc3rj.png",
+  "https://res.cloudinary.com/dfsozazph/image/upload/v1770360193/ozszvuqe2eyxdk6dngvl.png",
+  "https://res.cloudinary.com/dfsozazph/image/upload/v1770360076/z5mji4td6iztrvgfmdlu.png",
+];
+
 async function main() {
   // seed categories
   await prisma.category.createMany({
@@ -30,6 +42,7 @@ async function main() {
 
   // hash password
   const hashedPassword = await bcrypt.hash("admin123", 10);
+  const rand = Math.floor(Math.random() * AVATAR_PROFILES.length);
 
   // seed admin user
   await prisma.user.upsert({
@@ -39,6 +52,7 @@ async function main() {
       name: "Admin",
       email: "admin@gmail.com",
       password: hashedPassword,
+      image: AVATAR_PROFILES[rand],
     },
   });
 
